dfc7b7da4642f2f15a895b327996350151546366
mustafaaadil448 <mdmustafa8210@gmail.com>
2026-02-10 16:35:28 +0530

    Fix admin module import paths

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,26 +1,26 @@
-const Listing = require("../models/listing.js");
-const Review = require("../models/reviews.js");
-const User = require("../models/user.js");
+const Listing = require("./models/listing.js");
+const Review = require("./models/reviews.js");
+const User = require("./models/user.js");
 
 module.exports.dashboard = async (req, res) => {
-  const [totalListings, totalReviews, totalUsers] = await Promise.all([
-    Listing.countDocuments({}),
-    Review.countDocuments({}),
-    User.countDocuments({}),
-  ]);
+    const [totalListings, totalReviews, totalUsers] = await Promise.all([
+        Listing.countDocuments({}),
+        Review.countDocuments({}),
+        User.countDocuments({}),
+    ]);
 
-  const recentListings = await Listing.find({})
-    .sort({ _id: -1 })
-    .limit(5)
-    .populate("owner");
+    const recentListings = await Listing.find({})
+        .sort({ _id: -1 })
+        .limit(5)
+        .populate("owner");
 
-  const recentUsers = await User.find({})
-    .sort({ _id: -1 })
-    .limit(5);
+    const recentUsers = await User.find({})
+        .sort({ _id: -1 })
+        .limit(5);
 
-  res.render("admin/dashboard.ejs", {
-    stats: { totalListings, totalReviews, totalUsers },
-    recentListings,
-    recentUsers,
-  });
+    res.render("admin/dashboard.ejs", {
+        stats: { totalListings, totalReviews, totalUsers },
+        recentListings,
+        recentUsers,
+    });
 };
